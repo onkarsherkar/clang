c93a717720f5f6a62e457e6307eaba509bfcaaa8
onkarsherkar <onkar.sherkar@gmail.com>
2021-12-29 22:02:12 +0530

    variable update

--- HelloWorld.c
+++ HelloWorld.c
@@ -14,6 +14,9 @@ int main(){
   float gpa = 9.01;
   char grade = 'C'; //single character
   char name[] = "home";
-  printf("Your age is %d",age);
+  printf("Your age is %d\n",age);
+  printf("Hello %s\n", name);
+  printf("Your average grade is %c",grade);
+  printf("Your gpa is %f", gpa);
   return 0;
 }
