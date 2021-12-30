cad5b8ddf56efb4f31dc75009d0cef8abef7fa74
onkarsherkar <onkar.sherkar@gmail.com>
2021-12-30 21:06:20 +0530

    Arithmetic operations

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 
 int main(){
-  // constant value is fixed throughout the code.
+  /// Arithmetic Operators
 
-  float PI = 3.1416;
-  printf("%f", PI);
+  // + addition
+  // - substraction
+  // * multiplication
+  // / division
+  // % Moodulus
+  // ++ increment
+  // -- decrement
+
+  int x = 5;
+  int y = 2;
+
+  int z = x + y;
+  
+
+  printf("Addition of %d and %d is %d\n", x, y, z);
+
+  z = x - y;
+  printf("Substraction of %d and %d is %d\n", x, y,z);
+
+  z = x * y;
+  printf("Multiplication od %d and %d is %d\n", x, y, z);
+
+  z = x / y;
+  printf("Division of %d and %d is %d\n", x, y, z);
+
+  z = x % y;
+  printf("Modulus of %d and %d is %d\n", x, y, z);
+
+  x++;
+  printf("Increment of x is %d\n", x);
+
+  y--;
+  printf("Decrement of y is %d\n", y);
 
   return 0;
 }
