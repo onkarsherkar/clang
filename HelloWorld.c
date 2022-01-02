434d9e087d252513421b04fe9873f3e606d68d1d
onkarsherkar <onkar.sherkar@gmail.com>
2022-01-02 15:29:36 +0530

    Math Function

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include<math.h>
 
 int main(){
   
-  /// User Input
+  /// Math Function
 
-  int age;
-  char name [20]; 
+  double A = sqrt(9);
+  double B = pow(2, 4);
+  int C = round(3.25);
+  int D = ceil(4.33);
+  int E = floor(5.55);
+  double F = fabs(-25);
+  double G = log(3);
+  double H = sin(45);
+  double I = cos(45);
+  double J = tan(45);
 
-  printf("Enter your name\n");
-  fgets(name, 20, stdin);
-  printf("Name is %s",name);
-  printf("Enter your age\n");
-  scanf("%d", &age);
-
+  printf("%0.2lf\n", J);
 
   return 0;
 }
